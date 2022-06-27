02011e780f02bd68112744e00f8370ea9daa516c
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-06-27 08:01:56 -0300

    Create 03.py

---
+++ Mundo1/03.py
@@ -0,0 +1,6 @@
+#Crie um programa que leia dois números e mostre a soma entre eles
+
+num1 = int(input("Insira o primeiro número:\n"))
+num2 = int(input("Insira o segundo número:\n"))
+
+print("O resultado da soma entre os dois valores é de: {}".format(num1 + num2))
