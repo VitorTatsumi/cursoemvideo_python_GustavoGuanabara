9a171aa4942d373759a030a252057f37d6e4ae63
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-06-27 15:44:55 -0300

    Create 05.py

---
+++ Mundo1/05.py
@@ -0,0 +1,4 @@
+#Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor e seu antecessor
+
+num = int(input('Insira um número:\n'))
+print('O número inserido foi: {}\nO sucessor é: {}\nO antecessor é: {}\n'.format(num, num + 1, num - 1))
