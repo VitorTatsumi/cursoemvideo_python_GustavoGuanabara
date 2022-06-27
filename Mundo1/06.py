b341842937ce4975117af07d0d4c282c91b00632
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-06-27 15:42:12 -0300

    Create 06.py

---
+++ Mundo1/06.py
@@ -0,0 +1,4 @@
+#Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+num = int(input('Insira um número:\n'))
+print('O número inserido foi: {}\nO dobro do número é: {}\nO triplo do número é: {}\nA raíz quadrada do número é: {}\n'.format(num, num * 2, num * 3, num * num))
