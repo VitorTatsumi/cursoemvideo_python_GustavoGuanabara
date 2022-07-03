6537a21021ce902d93dcabfb549f9952a861ad84
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-07-03 15:03:53 -0300

    Update 35.py

--- Mundo1/35.py
+++ Mundo1/35.py
@@ -1,8 +1,8 @@
 #Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo
 
-a = int(input('Insira a reta: '))
-b = int(input('Insira a reta: '))
-c = int(input('Insira a reta: '))
+a = int(input('Insira um comprimento de reta: '))
+b = int(input('Insira um comprimento de reta: '))
+c = int(input('Insira um comprimento de reta: '))
 
 if a + b > c and b + c > a and a + c > b:
   print('Há condições para a existência de um triângulo.')
