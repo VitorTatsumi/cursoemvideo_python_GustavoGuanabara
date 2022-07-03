04e15d18722128cb2a10a2f8eac5aa7e9d0679ea
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-07-03 09:57:21 -0300

    Create 32.py

---
+++ Mundo1/32.py
@@ -0,0 +1,14 @@
+#Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
+
+ano = int(input('Insira um ano: \n'))
+
+if ano % 4 == 0:
+  if ano % 100 == 0:
+    if ano %400 == 0:
+      print('O ano é bissexto.')
+    else:
+      print('O ano não é bissexto.')
+  else:
+    print('O ano é bissexto.')
+elif ano % 4 != 0:
+  print('O ano não é bissexto.')
