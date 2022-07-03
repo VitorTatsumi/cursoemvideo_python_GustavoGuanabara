6ecd74c956e6e27d4caa96ebbac1356dd77e2cff
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-07-03 08:37:12 -0300

    Create 30.py

---
+++ Mundo1/30.py
@@ -0,0 +1,8 @@
+#Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR
+
+num = int(input('Insira um número inteiro: \n'))
+
+if num % 2 == 0:
+  print('Número PAR.')
+else: 
+  print('Número ÍMPAR.')
