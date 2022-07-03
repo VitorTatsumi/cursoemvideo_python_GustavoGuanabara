d5092a4cd3c822cc19558510e05cfab65cb39a5f
VitorTatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-07-03 08:16:14 -0300

    Update 28.py

--- Mundo1/28.py
+++ Mundo1/28.py
@@ -2,11 +2,14 @@
 
 from random import randint
 
-numPC = randint(0, 5)
+#numPC = randint(0, 5)
 
 num = int(input('Tente descobrir qual o número que o computador pensou!\nInsira um número: \n'))
 
-if numPC == num:
-  print('Você acertou!\nO número pensado pela máquina foi {}!\n'.format(numPC))
+if randint(0,5) == num:
+#if numPC == num:
+  print('Você acertou!')
+  #print('O número pensado pela máquina foi: {}\n'.format(numPC))
 else:
-  print('Você errou!\nO número pensado pela máquina foi {}!'.format(numPC))
+  print('Você errou!')
+  #print('O número pensado pela máquina foi: {}\n'.format(numPC))
