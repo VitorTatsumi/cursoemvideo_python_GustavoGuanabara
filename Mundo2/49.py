bac52c8fecb9efb4ffed60fa61d8cf06d3d0f763
Vitor Tatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2022-07-12 14:51:31 -0300

    Create 49.py

---
+++ Mundo2/49.py
@@ -0,0 +1,8 @@
+'''
+Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando um laço for
+'''
+
+num = int(input('Insira um número para exibir sua tabuada: '))
+
+for c in range (1, 11):
+  print('{} X {} = {}'.format(c, num, c*num))
